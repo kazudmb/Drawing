37bcf8a1f217c2aca987c198809a4fec015f47dc
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-06-23 09:58:26 +0900

    add Project 9, part 3

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,82 +9,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var rows = 4
+    @State private var columns = 4
 
     var body: some View {
-        ZStack {
-            VStack {
-                ColorCyclingCircle(amount: self.colorCycle)
-                    .frame(width: 300, height: 300)
-                
-
-                Slider(value: $colorCycle)
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = 8
+                    self.columns = 16
+                }
             }
-        }
-        .drawingGroup()
     }
 }
-
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: CGFloat(value))
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.5)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
-            }
-        }
-    }
-
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
-
-        if targetHue > 1 {
-            targetHue -= 1
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+           AnimatablePair(Double(rows), Double(columns))
         }
 
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
     }
-}
-
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
-
-    // How wide to make each petal
-    var petalWidth: Double = 100
 
     func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
         var path = Path()
 
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
 
-            // move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
 
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
-
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-
-            // add it to our main path
-            path.addPath(rotatedPetal)
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
         }
 
-        // now send the main path back
         return path
     }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
